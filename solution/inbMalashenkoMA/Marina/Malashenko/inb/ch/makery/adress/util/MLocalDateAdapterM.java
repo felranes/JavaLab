0a2da9c8e36243fd1f05fe4cf9d5dedd5cd84a28
unknown <vp-8-504-20@vp-8-504-20.KLASS.local>
2015-05-21 16:18:25 +0300

    AddressBook

---
+++ solution/inbMalashenkoMA/Marina/Malashenko/inb/ch/makery/adress/util/MLocalDateAdapterM.java
@@ -0,0 +1,17 @@
+package Marina.Malashenko.inb.ch.makery.adress.util;
+
+import java.time.LocalDate;
+import javax.xml.bind.annotation.adapters.XmlAdapter;
+
+public class MLocalDateAdapterM extends XmlAdapter<String, LocalDate> {
+
+    @Override
+    public LocalDate unmarshal(String v) throws Exception {
+        return LocalDate.parse(v);
+    }
+
+    @Override
+    public String marshal(LocalDate v) throws Exception {
+        return v.toString();
+    }
+}
