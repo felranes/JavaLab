1ae75bee7c7390740956fe66d9635665f1e4c2b4
Lorenzo <leo.fadyushin@yandex.ru>
2015-06-20 20:32:08 +0300

    che nit napisal

---
+++ solution/ivt_2_Fadyushin_Alexey/AdressApp/build/project/src/Alexey/Fadyushin/Ivt/ch/makery/adress/model/FPersonListWrapperA.java
@@ -0,0 +1,26 @@
+package Alexey.Fadyushin.Ivt.ch.makery.adress.model;
+
+import java.util.List;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ * Helper class to wrap a list of persons. This is used for saving the
+ * list of persons to XML.
+ * 
+ * @author Marco Jakob
+ */
+@XmlRootElement(name = "persons")
+public class FPersonListWrapperA {
+
+    private List<FPersonA> persons;
+
+    @XmlElement(name = "person")
+    public List<FPersonA> getPersons() {
+        return persons;
+    }
+
+    public void setPersons(List<FPersonA> persons) {
+        this.persons = persons;
+    }
+}
